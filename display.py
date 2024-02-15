217ff2c0937f76239bb9536c83b04e19ebb184c7
Hani <Agguinihani.off@gmail.com>
2024-02-15 11:24:57 +0100

    implementation du main

--- display.py
+++ display.py
@@ -5,28 +5,45 @@ def calcul_surface_entre_deux_points(p1: Point, p2: Point):
     print("calcul de distance entre",p1.name," et ",p2.name," : ")
     print(p1.distance(p2))
 
-p1 = Point("A", 1, 2)
-p2 = Point("B", 5, 5)
-calcul_surface_entre_deux_points(p1, p2)
+def extract_details(user_input: str) -> tuple[str, float, float]:
+    # Split the expression by '=' to separate the name and the point creation
+    name, point_str = user_input.split('=')
+
+    # Strip whitespace from name
+    name = name.strip()
+
+    # Remove leading and trailing whitespaces and split the point string by ';'
+    sep = ';' if ';' in point_str else ','
+    point_str = point_str.strip()[len('Point('):-1]
+    x_str, y_str = point_str.split(sep)
+
+    try:
+        x = float(x_str)
+        y = float(y_str)
+        return name, x, y
+    except ValueError:
+        raise ValueError("Les coordonnées doivent être des nombres")
+
+def lire_point_teminal() -> Point:
+    user_input = input(f"Declarer un point nom=point(x,y) \n")
+    name, x, y = extract_details(user_input)
+    return Point(name, x, y)
+
+def calcul_surface_carrer(carrer: Carre):
+    print("calcul de surface de ",carrer.name," : ",carrer.surface())
 
 def lire_square():
-    print("Entrez le nom et la longueur du côté du carré dans le format suivant : Nom = Carre(cote)")
-    input_str = input("Nom et côté : ")
+    print("Declarer un carre nom=carre(x)")
+    input_str = input()
     nom, reste = input_str.split("=")
     cote = reste.split("(")[1].split(")")[0]
     cote = cote.strip()
     if not cote.isdigit():
         raise ValueError("La longueur du côté doit être un nombre.")
     cote = float(cote)
-    return nom.strip(), cote
+    carre=Carre(nom, cote)
 
-nom_du_carre, cote_du_carre = lire_square()
-print("Nom du carré :", nom_du_carre)
-print("Longueur du côté du carré :", cote_du_carre)
+    return carre
 
-def calcul_surface_carrer(carrer: Carre):
-    print("calcul de surface de ",carrer.name," : ",carrer.surface())
 
-carrer = Carre("carrer1", 5)
-calcul_surface_carrer(carrer)
 
